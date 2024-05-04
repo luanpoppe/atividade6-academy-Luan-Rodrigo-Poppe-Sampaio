8950d2f6387ced576efcdc8529da68d0d4900a1c
luanpoppe <luanpoppe@gmail.com>
2024-05-04 15:25:11 -0300

    feat: configurando o cypress cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
+const cucumber = require("cypress-cucumber-preprocessor").default
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.feature",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("file:preprocessor", cucumber())
     },
   },
 });
