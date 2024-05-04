ada28fdc4e3723116c3efcc117d2c3f7cb1c471a
luanpoppe <luanpoppe@gmail.com>
2024-05-04 18:15:47 -0300

    feat: terminei parte de listagem de usuários e comecei parte de detalhes de usuário

--- cypress/e2e/listar-usuarios.feature
+++ cypress/e2e/listar-usuarios.feature
@@ -17,10 +17,48 @@ Funcionalidade: Listar usuários
         Então o total de páginas deve ser 03
         E botão de avançar página deve estar habilitado, e o de voltar deve estar desabilitado
         E o total de usuário na página deve ser 06
-    @only
+    
     Cenário: Acessando página que mostra os usuários com 03 usuários cadastrados
         E que há 03 usuários cadastrados
         Então ao acessar a página /users o total de páginas deverá ser 01
         E botões de avançar e voltar página devem estar desabilitados
         E o total de usuário na página deve ser 03
+    
+    Cenário: Acessando página que mostra os usuários com nenhum usuário cadastrado
+        E que não há usuários cadsatrados
+        Então a página deverá ter um título com o texto "Ops! Não existe nenhum usuário para ser exibido."
+        E a apágina deverá ter um botão com o texto "Cadastre um novo usuário"
+        E a página não deverá ter informações sobre usuários
+        E o botão de cadastrar usuário deverá redirecionar à URL correta
+    
+    @testeComBotoes
+    Cenário: Botão de avançar página deve funcionar corretamente
+        E que há mais de 06 usuários cadastrados
+        Quando clico no botão de avançar página
+        Então o site deverá ir para a página 02
+        E o site deverá mostrar as informações corretas dos usuários da página 02
 
+    @testeComBotoes
+    Cenário: Ao clicar duas vezes no botão de avançar, deverá aser mostrado a página 03
+        E que há mais de 06 usuários cadastrados
+        Quando clico no botão de avançar página 02 vezes
+        Então o site deverá ir para a página 03
+        E o botão de avançar deverá ficar desabilitado
+        E o site deverá mostrar as informações corretas dos usuários da página 03
+    
+    @testeComBotoes
+    Cenário: Botão de voltar página deve funcionar corretamente
+        E que há mais de 06 usuários cadastrados
+        E que estou na página 02
+        Quando clico no botão de voltar página
+        Então o usuário deverá estar na página 01
+        E o botão de voltar deverá estar desabilitado
+        E os usuários mostrados deverão ser os corretos da página 01
+    
+    Cenário: Botão de Home do header do site deve redirecionar à página inicial
+        Quando clico no botão de home do header
+        Então deverei ser redirecionado à página inicial
+
+    Cenário: Botão de cadastrar novo usuário do header do site deve redirecionar à página de cadastrar usuários
+        Quando clico no botão de cadastrar novo usuário do header do site
+        Então deverei ser redirecionado à página de cadastrar usuários
--- cypress/support/steps/listar-usuarios.step.js
+++ cypress/support/steps/listar-usuarios.step.js
@@ -3,9 +3,16 @@ import { PaginaInicial } from "../pages/pagina-inicial"
 
 const paginaInicial = new PaginaInicial()
 const baseUrl = "https://rarocrud-frontend-88984f6e4454.herokuapp.com"
+let nextPage
+let previousPage
 
 Before(function () {
     cy.viewport("macbook-13")
+})
+
+Before({ tags: "@testeComBotoes" }, function () {
+    nextPage = () => cy.get(paginaInicial.buttonNextPage).click()
+    previousPage = () => cy.get(paginaInicial.buttonPreviousPage).click()
 })
 
 Given("que visitei a página inicial", function () {
@@ -34,6 +41,51 @@ Given('que há 03 usuários cadastrados', function () {
     }).as("getUsers")
 
     cy.wait("@getUsers")
+})
+
+Given('que não há usuários cadsatrados', function () {
+    cy.intercept("GET", "api/v1/users", {
+        body: []
+    }).as("getUsers")
+
+    cy.wait("@getUsers")
+})
+
+Given('que há mais de 06 usuários cadastrados', function () {
+    cy.intercept("GET", "api/v1/users", {
+        fixture: "get-usuarios/get-18-usuarios.json"
+    }).as("getUsers")
+
+    cy.wait("@getUsers")
+})
+
+Given('que estou na página 02', function () {
+    nextPage()
+})
+
+
+
+
+
+When('clico no botão de avançar página', function () {
+    nextPage()
+})
+
+When('clico no botão de avançar página 02 vezes', function () {
+    nextPage()
+    nextPage()
+})
+
+When('clico no botão de voltar página', function () {
+    previousPage()
+})
+
+When('clico no botão de home do header', function () {
+    cy.get(paginaInicial.buttonHome).click()
+})
+
+When('clico no botão de cadastrar novo usuário do header do site', function () {
+    cy.get(paginaInicial.buttonNovoUsuario).click()
 })
 
 
@@ -73,4 +125,77 @@ Then('botão de avançar página deve estar habilitado, e o de voltar deve estar
 
 Then('o total de usuário na página deve ser 03', function () {
     cy.get(paginaInicial.itensListaUsuarios).should("have.length", 3)
+})
+
+Then('a página deverá ter um título com o texto {string}', function (texto) {
+    cy.get("h3").should('have.text', texto)
+})
+
+Then('a apágina deverá ter um botão com o texto {string}', function (texto) {
+    const cadastrarNovoUsuario = "[href='/users/novo']"
+
+    cy.get(".sc-koXPp.csBRDe " + cadastrarNovoUsuario + " p").should('have.text', texto)
+})
+
+Then('a página não deverá ter informações sobre usuários', function () {
+    cy.get(paginaInicial.itensListaUsuarios).should("not.exist")
+})
+
+Then('o botão de cadastrar usuário deverá redirecionar à URL correta', function () {
+    const cadastrarNovoUsuario = "[href='/users/novo']"
+
+    cy.get(".sc-koXPp.csBRDe " + cadastrarNovoUsuario).click()
+    cy.url().should("equal", baseUrl + "/users/novo")
+})
+
+Then('o site deverá ir para a página 02', function () {
+    cy.get(paginaInicial.itemPaginacaoAtual).should('have.text', "2 de 3")
+})
+
+Then('mostrar as informações corretas dos usuários da página 02 as informações corretas dos usuários da página 02', function () {
+    cy.get(paginaInicial.itensListaUsuarios).then(function (lista) {
+        for (let item of lista) {
+            cy.wrap(item).should('contain', "Página 02")
+        }
+    })
+})
+
+Then('o site deverá ir para a página 03', function () {
+    cy.get(paginaInicial.itemPaginacaoAtual).should('have.text', "3 de 3")
+})
+
+Then('o botão de avançar deverá ficar desabilitado', function () {
+    cy.get(paginaInicial.buttonNextPage).should('be.disabled')
+})
+
+Then('o site deverá mostrar as informações corretas dos usuários da página 03', function () {
+    cy.get(paginaInicial.itensListaUsuarios).then(function (lista) {
+        for (let item of lista) {
+            cy.wrap(item).should('contain', "Página 03")
+        }
+    })
+})
+
+Then('o usuário deverá estar na página 01', function () {
+    cy.get(paginaInicial.itemPaginacaoAtual).should('have.text', "1 de 3")
+})
+
+Then('o botão de voltar deverá estar desabilitado', function () {
+    cy.get(paginaInicial.buttonPreviousPage).should('be.disabled')
+})
+
+Then('os usuários mostrados deverão ser os corretos da página 01', function () {
+    cy.get(paginaInicial.itensListaUsuarios).then(function (lista) {
+        for (let item of lista) {
+            cy.wrap(item).should('contain', "Página 01")
+        }
+    })
+})
+
+Then('deverei ser redirecionado à página inicial', function () {
+    cy.url().should("equal", baseUrl + "/users")
+})
+
+Then('deverei ser redirecionado à página de cadastrar usuários', function () {
+    cy.url().should("equal", baseUrl + "/users/novo")
 })
