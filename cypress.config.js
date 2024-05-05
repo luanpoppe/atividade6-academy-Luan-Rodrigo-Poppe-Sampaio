358d8c547dd44c7f00aad4b5751d43961740b78f
luanpoppe <luanpoppe@gmail.com>
2024-05-05 16:29:51 -0300

    fix do arquivo corrompido

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     baseUrl: "https://rarocrud-frontend-88984f6e4454.herokuapp.com/",
     env: {
       apiUrl: "https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1/",
-      TAGS: "@only",
+      // TAGS: "@only",
     },
     specPattern: "cypress/e2e/**/*.feature",
     setupNodeEvents(on, config) {
